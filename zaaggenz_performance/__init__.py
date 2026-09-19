40051c1465fcdae9ecda335c1754d7a21715ffde
tom <261801702+techrote@users.noreply.github.com>
2026-09-19 13:12:01 +0100

    ZG-042: export authoritative sequence memory bound

--- zaaggenz_performance/__init__.py
+++ zaaggenz_performance/__init__.py
@@ -3,7 +3,8 @@
 from .policy import (
     POLICY_ID, POLICY_VERSION, PerformanceError, WorkloadEstimate, ChunkPolicy,
     chunk_policy, require_chunk_count, estimate_workload, estimate_longform_bars,
-    bar_frame_count, admission_memory, recommended_section_bars,
+    bar_frame_count, admission_memory, persistent_sequence_memory_bound,
+    recommended_section_bars,
 )
 from .sections import (
     LayerSection, PersistentSequenceResult, section_frame_count,
@@ -14,7 +15,8 @@ from .sections import (
 __all__ = [
     "POLICY_ID", "POLICY_VERSION", "PerformanceError", "WorkloadEstimate", "ChunkPolicy",
     "chunk_policy", "require_chunk_count", "estimate_workload", "estimate_longform_bars",
-    "bar_frame_count", "admission_memory", "recommended_section_bars",
+    "bar_frame_count", "admission_memory", "persistent_sequence_memory_bound",
+    "recommended_section_bars",
     "LayerSection", "PersistentSequenceResult", "section_frame_count",
     "estimate_persistent_sequence", "render_persistent_sections",
     "submit_persistent_sections",
