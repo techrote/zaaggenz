e59969d88e30178c2f33f9036d3344116699601a
tom <261801702+techrote@users.noreply.github.com>
2026-09-19 14:12:52 +0100

    ZG-040: route public study API through corrective trust boundary

--- zaaggenz_studies/__init__.py
+++ zaaggenz_studies/__init__.py
@@ -2,14 +2,14 @@
 
 from .model import (
     VERSION, FAMILIES, MODES, SCALES, METHODS, StudyError, StudyManifest,
-    FrozenStudy, StudyAmendment, StudyDeviation, freeze_manifest,
+    FrozenStudy, StudyAmendment, StudyDeviation,
     verify_frozen_manifest, changed_paths, make_amendment,
     verify_amendment_chain, make_deviation,
 )
-from .analysis import StudyDataset, analyse_study
-from .design import (
-    SimulationAssumptions, balanced_cyclic_orders, simulate_crossed_design,
-    plan_precision,
+from .corrective import (
+    StudyDataset, StudyEvidencePlan, freeze_evidence_plan, analyse_study,
+    freeze_manifest, SimulationAssumptions, balanced_cyclic_orders,
+    simulate_crossed_design, plan_precision,
 )
 
 __all__ = [
@@ -17,6 +17,7 @@ __all__ = [
     "StudyManifest", "FrozenStudy", "StudyAmendment", "StudyDeviation",
     "freeze_manifest", "verify_frozen_manifest", "changed_paths",
     "make_amendment", "verify_amendment_chain", "make_deviation",
-    "StudyDataset", "analyse_study", "SimulationAssumptions",
-    "balanced_cyclic_orders", "simulate_crossed_design", "plan_precision",
+    "StudyDataset", "StudyEvidencePlan", "freeze_evidence_plan",
+    "analyse_study", "SimulationAssumptions", "balanced_cyclic_orders",
+    "simulate_crossed_design", "plan_precision",
 ]
